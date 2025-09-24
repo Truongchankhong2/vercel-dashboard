55ab543cc971482427d671a7985b39736139f267
Production Public <prod.public@ortholite.local>
2025-09-24 22:01:29 +0700

    Auto NIGHT update at 2025-09-24 22:01:28

---
+++ public/supabaseClient.node.js
@@ -0,0 +1,8 @@
+// Supabase client cho Node
+import { createClient } from "@supabase/supabase-js";
+
+const SUPABASE_URL = "https://ixdtdrbytwdmnlqgunzu.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml4ZHRkcmJ5dHdkbW5scWd1bnp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMyMzkyODYsImV4cCI6MjA2ODgxNTI4Nn0.5FLdLDf0d1yA70UBmAbJYW95kVWdta31QmEjm9oX4jg";
+
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
