d2bd78bc08f7f0e521238bcfc6671fb760023757
Production Public <prod.public@ortholite.local>
2025-12-06 18:49:14 +0700

    Auto update from NIGHT script

--- public/script.js
+++ public/script.js
@@ -1,7 +1,8 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+// Lấy createClient từ global supabase (đã load bằng <script> trong index.html)
+const { createClient } = window.supabase;
 
 // Khai báo Supabase
-const supabaseUrl = 'https://ixdtdrbytwdmnlqgunzu.supabase.co'; // URL của bạn
+const supabaseUrl = 'https://ixdtdrbytwdmnlqgunzu.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml4ZHRkcmJ5dHdkbW5scWd1bnp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMyMzkyODYsImV4cCI6MjA2ODgxNTI4Nn0.5FLdLDf0d1yA70UBmAbJYW95kVWdta31QmEjm9oX4jg'; // Anon key từ Supabase
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
